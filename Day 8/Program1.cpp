c2c921a10a3567db4a1de08f1eb3508848c65d43
Vimal M <109758195+Vimall03@users.noreply.github.com>
2023-05-19 20:41:44 +0530

    Update Program1.cpp

--- Day 8/Program1.cpp
+++ Day 8/Program1.cpp
@@ -4,16 +4,16 @@ using namespace std;
 void printPattern(int n){
     int i, j, k;
     for(i = 0; i<n ;i++){
-        //space
+        //prints space
         for( j=0; j<n-i; j++)
         {
             cout<<" ";
         }
-        //star
+        //prints star
         for(k=0; k<2*i +1; k++){
             cout<<"*";
         }
-        //space
+        //prints space
         for( j=0; j<n-i; j++)
         {
             cout<<" ";
